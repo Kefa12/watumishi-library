db5e2b2574441c0807265889509e4b916fcb90d4
kefa12 <msigwakefa@gmail.com>
2022-07-28 16:06:56 +0300

    home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
   <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="index.html"><img scr="assets/img/logo.png">WL</img></a></h1>
+      <h1 class="logo me-auto"><a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid" width=100px  height=100px></img></a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
@@ -85,7 +85,7 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-          <h1>Watumishi Library Service</h1>
+          <h1>Watumishi Library Ministries</h1>
           <h2>We are team of talented designers making service better</h2>
           <div class="d-flex justify-content-center justify-content-lg-start">
             <a href="#about" class="btn-get-started scrollto">Get Started</a>
